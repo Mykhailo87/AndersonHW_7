957c0f3ec8383ed868b68ac906445e7f9bc5947c
Mykhailo87 <64310136+Mykhailo87@users.noreply.github.com>
2024-01-15 00:41:55 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,91 @@
+const display = document.querySelector(".display");
+
+let previousInput = '';
+let currentInput = '';
+let operator = '';
+
+function appendToDisplay(value) {
+    if (value === '.' && currentInput.includes('.')) {
+        return;
+    }
+
+    currentInput += value.toString();
+    updateDisplay();
+}
+
+function deleteLastNumber() {
+    if (currentInput.length > 0) {
+        currentInput = currentInput.slice(0, -1);
+    }
+    
+    updateDisplay();
+}
+
+function updateDisplay() {
+    if (currentInput) {
+        display.innerText = currentInput;
+    } 
+}
+
+function changeToOpposite() {
+  currentInput = (-(+currentInput)).toString();
+  updateDisplay();
+}
+
+function clearDisplay() {
+    currentInput = '';
+    previousInput = '';
+    operator = '';
+    display.innerText = '';
+}
+
+function setOperator(newOperator) {
+    if (currentInput === '') {
+      return;
+    }
+    if (previousInput !== '') {
+      calculate();
+    }
+
+    operator = newOperator;
+    previousInput = currentInput;
+    currentInput = '';
+    updateDisplay();
+}
+
+
+function calculate() {
+    let calculation;
+    const firstOperand = parseFloat(previousInput);
+    const secondOperand = parseFloat(currentInput);
+    
+    if (isNaN(firstOperand) || isNaN(secondOperand)) {
+      clearDisplay();
+      return;
+    }
+
+    switch (operator) {
+      case '+':
+        calculation = (firstOperand + secondOperand).toFixed(8).replace(/\.?0+$/, '');
+        break;
+      case '-':
+        calculation = (firstOperand - secondOperand).toFixed(8).replace(/\.?0+$/, '');
+        break;
+      case '*':
+        calculation = (firstOperand * secondOperand).toFixed(8).replace(/\.?0+$/, '');
+        break;
+      case '/':
+        if (secondOperand === 0) {
+          clearDisplay();
+          display.innerText = 'Error';
+          return;
+        }
+        calculation = (firstOperand / secondOperand).toFixed(8).replace(/\.?0+$/, '');
+        break;
+    }
+    
+    currentInput = calculation;
+    operator = '';
+    previousInput = '';
+    updateDisplay();
+}
